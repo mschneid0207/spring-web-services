66d26b7f6f213adad34e874e9a265de5b7288801
mschneid0207 <markus.schneider0207@gmail.com>
2018-06-29 21:28:22 +0200

    project setup

---
+++ src/main/java/de/mschneid/ms/restfulwebservices/RestfulWebServicesApplication.java
@@ -0,0 +1,12 @@
+package de.mschneid.ms.restfulwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulWebServicesApplication.class, args);
+	}
+}
