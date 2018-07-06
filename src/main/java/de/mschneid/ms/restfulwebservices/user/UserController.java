e45d8032c675dc4b61d7e2ed96ffdb75e1103ac6
mschneid0207 <markus.schneider0207@gmail.com>
2018-07-06 15:32:31 +0200

    user not found exception 404

--- src/main/java/de/mschneid/ms/restfulwebservices/user/UserController.java
+++ src/main/java/de/mschneid/ms/restfulwebservices/user/UserController.java
@@ -1,8 +1,11 @@
 package de.mschneid.ms.restfulwebservices.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -19,11 +22,22 @@ public class UserController {
 
     @GetMapping("/users/{id}")
     public User retrieveUser(@PathVariable int id) {
-        return userDaoService.findOne(id);
+        User user = userDaoService.findOne(id);
+        if (user == null) {
+            throw new UserNotFoundException("id-" +id);
+        }
+        return user;
     }
 
     @PostMapping("/users")
-    public void createUser(@RequestBody User user) {
+    public ResponseEntity createUser(@RequestBody User user) {
        User savedUser = userDaoService.save(user);
+
+       URI location = ServletUriComponentsBuilder
+               .fromCurrentRequest()
+               .path("/{id}")
+               .buildAndExpand(savedUser.getId()).toUri();
+
+        return ResponseEntity.created(location).build();
     }
 }
