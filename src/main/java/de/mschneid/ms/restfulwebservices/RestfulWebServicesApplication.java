1b7e7dde0c385f072911a4da9adefe99981f567d
mschneid0207 <markus.schneider0207@gmail.com>
2018-08-02 13:26:12 +0200

    simplify internationalization

--- src/main/java/de/mschneid/ms/restfulwebservices/RestfulWebServicesApplication.java
+++ src/main/java/de/mschneid/ms/restfulwebservices/RestfulWebServicesApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import java.util.Locale;
@@ -18,7 +19,8 @@ public class RestfulWebServicesApplication {
 
 	@Bean
 	public LocaleResolver localeResolver() {
-		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
+		//SessionLocaleResolver localeResolver = new SessionLocaleResolver();
 		localeResolver.setDefaultLocale(Locale.US);
 		return localeResolver;
 	}
