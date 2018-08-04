ab674b8c0d2e5945e9889a9d45b8f85fceae0f7b
mschneid0207 <markus.schneider0207@gmail.com>
2018-08-04 14:07:44 +0200

    static filtering

---
+++ src/main/java/de/mschneid/ms/restfulwebservices/filtering/SomeBean.java
@@ -0,0 +1,17 @@
+package de.mschneid.ms.restfulwebservices.filtering;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class SomeBean {
+
+    private String field1;
+    private String field2;
+    @JsonIgnore
+    private String field3;
+}
