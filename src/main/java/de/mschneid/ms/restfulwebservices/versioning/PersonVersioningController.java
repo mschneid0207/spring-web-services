5ec6b703d612e759b55c66b5c4cf5e4d92c45c95
Markus Schneider <markus.schneider0207@gmail.com>
2018-08-31 22:47:28 +0200

    different versioning approaches

--- src/main/java/de/mschneid/ms/restfulwebservices/versioning/PersonVersioningController.java
+++ src/main/java/de/mschneid/ms/restfulwebservices/versioning/PersonVersioningController.java
@@ -7,13 +7,33 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PersonVersioningController {
 
-    @GetMapping("v1/person")
+    @GetMapping(value = "/person/param", params = "version=1")
     public PersonV1 personV1() {
         return new PersonV1("Markus Schneider");
     }
 
-    @GetMapping("v2/person")
+    @GetMapping(value = "/person/param", params = "version=2")
     public PersonV2 personV2() {
+        return new PersonV2(new Name("Nadine", "Schneider"));
+    }
+
+    @GetMapping(value = "/person/header", headers = "X-API-VERSION=1")
+    public PersonV1 headerV1() {
+        return new PersonV1("Markus Schneider");
+    }
+
+    @GetMapping(value = "/person/header", headers = "X-API-VERSION=2")
+    public PersonV2 headerV2() {
+        return new PersonV2(new Name("Nadine", "Schneider"));
+    }
+
+    @GetMapping(value = "/person/produces", produces = "application/vnd.company.app-v1+json")
+    public PersonV1 producesV1() {
+        return new PersonV1("Markus Schneider");
+    }
+
+    @GetMapping(value = "/person/produces", produces = "application/vnd.company.app-v2+json")
+    public PersonV2 producesV2() {
         return new PersonV2(new Name("Nadine", "Schneider"));
     }
 }
--- src/main/resources/notes.md
+++ src/main/resources/notes.md
@@ -4,3 +4,14 @@ _`Statische Filterung`_
 `Dynamische Filterung`
 Methoden filtern unterschiedliche Variablen aus dem Response
 MappingJacksonValue ...
+
+`Versionierung`
+1. Möglichkeit: URI Versioning: v1/person v2/person in url 
+2. Möglichkeit: Request Param Versioning: über params z.B. /person?version=1 oder person?version=2
+3. Möglichkeit: Custom Header Versioning: über request header z.B. X-API-VERSION (key) und 2 (value)
+4. Möglichkeit: Media Type Versioning: über produces accept header z.B. application/vnd.company.app-v2+json
+
+Was muss man beachten!!!!
+1. 3 und 4 keine Möglichkeit vom Caching
+2. Dokumentation bei 3 und 4 nicht so einfach
+3. 3 und 4 nicht einfach aufzurufen über Browser
